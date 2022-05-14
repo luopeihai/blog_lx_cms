b32fc3ed40e77a9e59b960c0effdb7e25ab8c874
Peihai Luo （罗培海） <phluo@trip.com>
2022-05-14 10:27:15 +0800

    feat: logo修改

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="login">
-    <div class="team-name hidden-sm-and-down"><img src="@/assets/image/login/team-name.png" alt="logo" /></div>
+    <div class="team-name hidden-sm-and-down"></div>
     <div class="form-box" v-loading="loading" element-loading-background="rgba(0, 0, 0, 0)">
       <div class="title">
-        <h1 title="Lin">Lin CMS</h1>
+        <h1 title="Lin">blog CMS</h1>
       </div>
       <form class="login-form" autocomplete="off" @submit.prevent="throttleLogin()">
         <div class="form-item nickname">
